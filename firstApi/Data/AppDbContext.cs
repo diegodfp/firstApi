8cea04741c5797fa66358c7258307d7df1883c23
diegodfp <diegodfp.fly@gmail.com>
2024-11-29 02:52:01 -0500

    Agregar archivos de proyecto.

---
+++ firstApi/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using firstApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace firstApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
+        
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ firstApi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace firstApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
